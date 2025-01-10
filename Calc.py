d80d9f857d3e9fed1931784ae906a237b0ab3425
Ugyenjigmerangdrel <ugyen.jigme2018@gmail.com>
2025-01-10 16:21:41 +0530

    change in logic

--- Calc.py
+++ Calc.py
@@ -1,7 +1,43 @@
-print("Select operation.")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-print("5. Power")
+import Addition
+import Subtraction
+import Multiplication
+import Division
 
+# This is to present a menu to the user
+print("Select operation.")
+print("1.Addition")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", Addition.add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+
+        elif choice == '4':
+            if num2 == float(0):
+                print("The Dividend cannot be zero")
+            else:
+                print(num1, "/", num2, "=", Division.divide(num1, num2))
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
